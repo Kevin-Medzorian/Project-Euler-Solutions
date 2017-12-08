acf955e43ff7b6e0a1c0c07984c90674214b7571
Kevin Medzorian <Kevin.Medzor@gmail.com>
2017-12-08 13:52:06 -0600

    Updated problem 60

--- euler060.java
+++ euler060.java
@@ -1,32 +1,62 @@
 
 import java.util.ArrayList;
 
-
 public class euler060 extends Euler {
+
+    public static boolean[] primes;
+    public static ArrayList<Integer>[] test = new ArrayList[1_000_000];
 
     public static void main(String[] args) throws Exception {
         Start();
+
         final int MAX = 1_000_000;
 
-        boolean[] primes = sieve(MAX);
-
-        ArrayList<Integer> commonPrimes = new ArrayList();
+        primes = sieve(MAX);
+        test = new ArrayList[1_000_000];
+        for (int i = 1; i < MAX; i += 2) {
+            if (primes[i]) {
+                isPrime(i, i);
+            }
+        }
         
-        for (int i = 0; i < MAX; i++) {
-            if(primes[i]){
+        for (ArrayList<Integer> list : test) {
+            if(list != null)
+                System.out.println(list);
+        }
+    }
+
+    public static int isPrime(int num, int original) {
+        num *= 10;
+
+        int pow = (int) Math.log(num / (double) original);
+        int max = (int) Math.pow(10, pow);
+
+        for (int i = 1; i < max; i += 2) {
+            if(num + i < 1_000_000 && i < 1_000_000)
+            if (primes[i] && primes[num + i]) {
+               // System.out.println(i);
                 
-                int temp = i;
-                
-                if(primes[temp % 10]){
-                    commonPrimes.add(temp % 10);
+                int leftSide = i * (int) Math.pow(10, (int) Math.log(num) + 1) + original;
+                if (leftSide < 1_000_000 && leftSide >= 0  && primes[leftSide]) {
+                    if (test[i] == null) {
+                        test[i] = new ArrayList();
+                    }
+
+                    if (test[original] == null) {
+                        test[original] = new ArrayList();
+                    }
+
+                    test[i].add(original);
+                    test[original].add(i);
                 }
-                if(primes[(temp % 100) + (temp % 10)]){
-                    commonPrimes.add((temp % 100) + (temp % 10));
-                }
-                //brain storming
             }
         }
 
+        if (num < 1_000_000) {
+            return isPrime(num, original);
+        }
+
+        return 0;
     }
 
 }
